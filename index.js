fba135c31fff430405a40f025a4ed11a8773617e
ederson goncalves <edersonaugusto59@gmail.com>
2023-03-11 16:50:09 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const port = 8000;
+const bodyparser = require('body-parser');
+
+//CONFIGURANDO BODY-PARSER
+//app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+
+//CONFIGURANDO EJS
+app.set('view engine', 'ejs')
+
+//CONFIGURANDO ARQUIVOS ESTATICOS
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    return res.render("index")
+});
+
+
+//LOG
+app.listen(port, () => (
+    console.log('Servidor iniciado na porta' + port)
+));
