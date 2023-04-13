00bd239d9301c274b81b22195dc806714d8c46b5
ederson goncalves <edersonaugusto59@gmail.com>
2023-04-13 11:01:18 -0300

    variavel porta criada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const bodyparser = require('body-parser');
 
 //CONFIGURANDO BODY-PARSER
